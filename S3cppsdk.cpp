ac534f467d3f4d2ceec15362700536df6e6a13f0
sc703bupt <sc1_1@bupt.edu.cn>
2013-11-08 11:47:27 +0800

    add curl /include and libcurl.lib,set up an test case(not success yet)

--- S3cppsdk.cpp
+++ S3cppsdk.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <curl/curl.h>
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	CURL *curl; 
+	CURLcode res; 
+
+	curl = curl_easy_init(); 
+	if(curl) { 
+		curl_easy_setopt(curl, CURLOPT_URL, "http://www.cnn.com/"); 
+		res = curl_easy_perform(curl); 
+
+		/* always cleanup */ 
+		curl_easy_cleanup(curl); 
+	} 
 	return 0;
 }
 
